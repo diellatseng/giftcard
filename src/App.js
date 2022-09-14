f449b4b3b71cda58e45f53e6039713f0c88a6b66
TSENG Ching-Yao <ching-yao.tseng@teamwork.net>
2022-09-14 18:23:50 +0200

    color

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import { useState, useEffect } from 'react';
 import ReactCodeInput from 'react-code-input';
 import toast, { Toaster } from 'react-hot-toast';
 
+// Inline styles for input
 const colorPrimary = 'rgb(168, 21, 168)';
 const colorPrimaryText = 'rgb(255, 255, 255)';
-
+const colorError = 'rgba(255, 0, 85, .3)';
+const colorErrorText = 'rgb(255, 0, 85)';
 const props = {
 	inputStyle: {
 		color: `${colorPrimaryText}`,
-		border: `2px solid ${colorPrimary}`
+		border: `2px solid ${colorPrimary}`,
+		backgroundColor: `${colorPrimary}`
 	},
 	inputStyleInvalid: {
-		color: 'red',
-		border: '2px solid red'
+		color: `${colorErrorText}`,
+		border: `2px solid ${colorError}`,
+		backgroundColor: `${colorError}`
 	}
 }
 const CORRECT_PIN_CODE = "ETOILE";
-
 const PinCode = () => {
 	const [isPinCodeValid, setIsPinCodeValid] = useState(undefined);
 	const [pinCode, setPinCode] = useState("");
@@ -38,8 +41,15 @@ const PinCode = () => {
 	}, [pinCode]);
 	// Error message for invalid pin
 	useEffect(() => {
-		if (isPinCodeValid === false && pinCode.length === 6) {
-			toast.error("This didn't work.")
+		const messages = [
+			"Are you serious?",
+			"This won't work.",
+			"Want to try again?",
+			"Wrong."
+		]
+		const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+		if (isPinCodeValid === false && pinCode.length === 6) {  // !!!! passing through this func before isPinCodeValid is assigned with new value
+			toast.error(randomMessage)
 		}
 	}, [isPinCodeValid, pinCode]);
 
